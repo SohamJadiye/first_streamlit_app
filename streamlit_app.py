8e4be9d24b3fc5e6c0b9e05b6ebddb82faf93794
Soham Jadiye <114732272+SohamJadiye@users.noreply.github.com>
2024-02-22 11:41:40 +0530

    Update streamlit_app.py
    Added  More details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
-import streamlit
+import Streamlit
+
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oats')
+streamlit.text('kale spinach and rocket smoothie')
+streamlit.text('hard boiled free range egg')
